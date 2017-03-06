e091fee482aaa84bb497de87b18f54b6198ebb5f
Vladimir Borovic <vladimir.borovic@gmail.com>
2017-03-06 15:27:49 +0200

    Background service

--- app/src/main/java/com/github/borovic/photogallery/QueryPreferences.java
+++ app/src/main/java/com/github/borovic/photogallery/QueryPreferences.java
@@ -8,18 +8,35 @@ import android.preference.PreferenceManager;
  * Created by vborovic on 3/3/17.
  */
 
-public class QueryPreferences {
+class QueryPreferences {
     private static final String PREF_SEARCH_QUERY = "searchQuery";
+    private static final String PREF_LAST_RESULT_ID = "lastResultId";
 
-    public static String getStoredQuery(Context context) {
-        return getDefaultSharedPreferences(context).getString(PREF_SEARCH_QUERY, null);
+    static String getStoredQuery(Context context) {
+        return getString(context, PREF_SEARCH_QUERY);
+    }
+
+    private static String getString(Context context, String key) {
+        return getDefaultSharedPreferences(context).getString(key, null);
     }
 
     private static SharedPreferences getDefaultSharedPreferences(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context);
     }
 
-    public static void setStoredQuery(Context context, String query) {
-        getDefaultSharedPreferences(context).edit().putString(PREF_SEARCH_QUERY, query).apply();
+    static void setStoredQuery(Context context, String query) {
+        setString(context, PREF_SEARCH_QUERY, query);
+    }
+
+    private static void setString(Context context, String key, String value) {
+        getDefaultSharedPreferences(context).edit().putString(key, value).apply();
+    }
+
+    static String getLastResultId(Context context) {
+        return getString(context, PREF_LAST_RESULT_ID);
+    }
+
+    static void setLastResultId(Context context, String id) {
+        setString(context, PREF_SEARCH_QUERY, id);
     }
 }
